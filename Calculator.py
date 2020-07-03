a0435db1657776e26615d335d67634ed254c4549
Gal Shaharabani <galshar1@gmail.com>
2020-07-03 13:28:30 +0300

    issue with Repeat function

--- Calculator.py
+++ Calculator.py
@@ -2,14 +2,13 @@ from math import *
 
 
 class Calculator:
-
-    def __init__(self, num1, num2, actions):
-        self.actions = actions["+", "-", "*", "/", "s", "^", "c", "r"]
+    actions=["+", "-", "*", "/", "s", "^", "c", "=", "r"]
+    def __init__(self, num1, num2):
         self.num1 = num1
         self.num2 = num2
 
-    def load_actions(self):
-        return ["+", "-", "*", "/", "s", "^", "c", "r"]
+    def load_actions(actions):
+        return actions
 
     def add(number1, number2):
         print("Result: ")
@@ -48,6 +47,25 @@ class Calculator:
         print(result)
         return result
 
+    def repeat(number1, number2, action):
+        actions = Calculator.actions
+        last_action = None
+        for action in  actions:
+            last_action = action
+        if last_action == actions[0]:
+            Calculator.add(number1, number2)
+        elif action == actions[1]:
+            Calculator.subtract(number1, number2)
+        elif action == actions[2]:
+            Calculator.multiply(number1, number2)
+        elif action == actions[3]:
+            Calculator.divide(number1, number2)
+        elif action == actions[4]:
+            Calculator.square_root(number1)
+        elif action == actions[5]:
+            Calculator.power(number1, number2)
+        elif action == actions[6]:
+            Calculator.clear(number1, number2)
     # def redo(number2):
     #     num2 = 0
     #     return num2
--- Calculator_logic.py
+++ Calculator_logic.py
@@ -5,12 +5,11 @@ num1 = float(input("enter first number: "))
 num2 = 0
 action = input("action: ")
 valid_action = True
-actions = Calculator.load_actions(self=True)
-
+actions = Calculator.load_actions(Calculator.actions)
 if action == actions[4]:
     num2 =0
 
-while len(action) != 1:
+while len(action) != 1 or action not in actions:
     valid_action = False
     print("invalid action")
     action = input("enter a valid action: ")
@@ -23,19 +22,21 @@ if valid_action and action != actions[4]:
 
 
 def calculate(number1, act, number2, actions):
+        result = 0
         while act != actions[0] and act != actions[1] and act != actions[2] and act != actions[3] \
                 and act != actions[4] and act != actions[5] and act != actions[6] and act != actions[7]:
             print(str(act) + " is not an action")
             act = input("enter a valid action: ")
             number2 = float(input("enter another number: "))
+
         # for act in actions:
         #     i=0
-        #     if act != actions[i]:
+        #     if act not in actions:
         #         print(str(act) + " is not an action")
         #         act = input("enter a valid action: ")
-        #         n2 = float(input("enter another number: "))
+        #         number2 = float(input("enter another number: "))
         #     i+=1
-
+        # number2 = number2
         while True:
 
             if act == actions[0]:
@@ -52,35 +53,33 @@ def calculate(number1, act, number2, actions):
 
             elif act == actions[3]:
                 while number2 == 0:
-                    try:
-                        number2 = float(input("enter valid number: "))
-                        result = Calculator.divide(number1, number2)
-                    except ZeroDivisionError:
-                        print("ERROR: cannot divide by 0")
+                    print("ERROR: cannot divide by 0")
+                    number2 = float(input("enter valid number: "))
+                result = Calculator.divide(number1, number2)
 
 
             elif act == actions[4]:
-                Calculator.square_root(number1)
+                result=Calculator.square_root(number1)
 
 
 
             elif act == actions[5]:
-                Calculator.power(number1,number2)
+                result=Calculator.power(number1,number2)
 
 
             elif act == actions[6]:
                 Calculator.clear(number1,number2)
                 result = float(input("enter new number: "))
 
+            elif act == actions[7]:
+                result = Calculator.repeat(number1,number2,act)
 
-            # elif act == actions[7]:
-            #     n2 = redo(n2)
 
             number1 = result
 
-            if act != actions[7]:
+            if act != actions[8]:
                 act = input("another action: ")
-                if len(act) != 1:
+                if len(act) != 1 or act not in actions:
                     print("invalid action")
                     act = input("enter a valid action: ")
 
